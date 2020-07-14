8a28c4dbe36e7f5e38ce6350660bcd5e10033391
Gurgen Nersesyan <zukalt@gmail.com>
2020-07-14 17:23:32 +0400

    added simple landing page with chart

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,7 +13,7 @@ export class AppController {
   @ApiParam({name: 'fromTS', description: 'Time since when data to be returned Unix TS'})
   @ApiQuery({name: 'ps', description: 'Page Size', required: false})
   report(@Param('fromTS') fromTS: number, @Query('ps') pageSize = 24*60 ): Promise<TopicsEntity[]> {
-    return this.appService.getOccurrenceReport(+fromTS, +pageSize)
+    return this.appService.getOccurrenceReport(fromTS*1000, +pageSize)
   }
 
 }
