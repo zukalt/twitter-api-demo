3243bdf58a3376b05e08232a7c691fa3c6f293c3
Gurgen Nersesyan <zukalt@gmail.com>
2020-07-14 16:44:29 +0400

    added static file serve from public folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,14 @@ import { ScraperService } from './scraper/scraper.service';
 import { databaseConfig, appConfig } from './config';
 import { TweetsStorage } from './storage/tweets.storage';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig, appConfig],
